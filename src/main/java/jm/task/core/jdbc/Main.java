dc05a39d0fe9c1ee875cba833520d73324a7d37f
IsmailStv <erkenov@yandex.ru>
2023-07-20 14:38:29 +0300

    Java pre-project. Практическая задача 1.1.4

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,25 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
+    private final static UserService userService = new UserServiceImpl();
+
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        userService.createUsersTable();
+
+        userService.saveUser("Джордж", "Клуни", (byte) 62);
+        userService.saveUser("Майли", "Сайрус", (byte) 30);
+        userService.saveUser("Брэд", "Питт", (byte) 59);
+        userService.saveUser("Ариана", "Гранде", (byte) 30);
+
+        userService.removeUserById(2);
+
+        userService.getAllUsers();
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
     }
 }
